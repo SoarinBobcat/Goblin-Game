435307aaa785b67da4ff7faca55d6abf10dce1da
SoarinBobcat <125240893+SoarinBobcat@users.noreply.github.com>
2023-11-06 15:46:35 +0800

    whoops

--- Assets/Scripts/PlayerMain.cs
+++ Assets/Scripts/PlayerMain.cs
@@ -72,7 +72,7 @@ public class PlayerMain : MonoBehaviour
 
         if (Input.GetButton("Kick"))
         {
-
+            kickAnim = true;
         }
 
         player.Move(playerVel * Time.deltaTime);
